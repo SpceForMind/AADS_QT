6d7b871564a729dabf50e6056621c63fad85c3b1
SpceForMind <31548759+SpceForMind@users.noreply.github.com>
2018-11-05 17:59:44 +0300

    Update main.cpp

--- CR/main.cpp
+++ CR/main.cpp
@@ -6,15 +6,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    /*
-    AvlTree *root = nullptr;
-    insert(&root, "abc");
-    insert(&root, "bhj");
-    insert(&root, "jhre");
-    qDebug() << root;
-    qDebug() << root->left;
-    qDebug() << root->right;
-    */
     MainWindow w;
     w.show();
 
